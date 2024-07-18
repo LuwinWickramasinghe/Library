85bc211974ebda9f478e7d0c08c78e4f0c2230b7
iamluwin <luwin.anuka@gmail.com>
2024-07-19 00:58:17 +0530

    added Footer and Homepage Component

--- 03-frontend/library-project/src/App.tsx
+++ 03-frontend/library-project/src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
-import { Carousel } from './layouts/Homepage/Carousel';
-import { Heroes } from './layouts/Homepage/Heroes';
-import { LibraryServices } from './layouts/Homepage/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/Homepage/HomePage';
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heroes/>
-      <LibraryServices/>
+      <HomePage/>
+      <Footer/>
     </div>
   );
 }
 
-export default App;
--- 03-frontend/library-project/src/layouts/Homepage/components/Carousel.tsx
+++ 03-frontend/library-project/src/layouts/Homepage/components/Carousel.tsx
@@ -1,6 +1,5 @@
 import { ReturnBook } from "./ReturnBook";
 
-
 export const Carousel = () => {
   return (
     <div className="container mt-5" style={{ height: 550 }}>
