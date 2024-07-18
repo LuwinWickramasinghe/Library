8ab4ddee7ce4f7304e03dd06d81fe1ab2ae9775b
iamluwin <luwin.anuka@gmail.com>
2024-07-19 00:39:39 +0530

    added Hero, Libraryservices, ReturnBook components

---
+++ 03-frontend/library-project/src/layouts/Homepage/Heroes.tsx
@@ -0,0 +1,74 @@
+export const Heroes = () => {
+  return (
+    <div>
+      <div className="d-none d-lg-block">
+        <div className="row g-0 mt-5">
+          <div className="col-sm-6 col-md-6">
+            <div className="col-image-left"></div>
+          </div>
+
+          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+            <div className="ml-2">
+              <h1>What have you been reading?</h1>
+              <p className="lead">
+                The library team would love to know what you have been reading.
+                Share your favorite books, recent reads, and recommendations
+                with us to help build a vibrant reading community!
+              </p>
+
+              <a href="#" className="btn main-color btn-lg text-white">
+                Sign-up
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="row g-0">
+            <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+                <div className="ml-2">
+                    <h1>Our collection is always changing!</h1>
+                    <p className="lead">The library team would love to know what you have been reading. Share your favorite books, recent reads, and recommendations with us to help build a vibrant reading community! Try checking in daily as our collection is always changing!</p>
+                </div>
+            </div>
+            <div className="col-sm-6 col-md-6">
+                <div className="col-image-right"></div>
+            </div>
+        </div>
+      </div>
+      {/*Mobile heroes */}
+      <div className="d-lg-none">
+        <div className="container">
+            <div className="m-2">
+                <div className="col-image-left"></div>
+                <div className="mt-2">
+                <h1>What have you been reading?</h1>
+                <p className="lead">
+                    The library team would love to know what you have been reading.
+                    Share your favorite books, recent reads, and recommendations
+                    with us to help build a vibrant reading community!
+                </p>
+
+                <a href="#" className="btn main-color btn-lg text-white">
+                    Sign-up
+                </a>
+                </div>
+            </div>
+            <div className="m-2">
+                <div className="col-image-right"></div>
+                <div className="mt-2">
+                    <h1>What have you been reading?</h1>
+                    <p className="lead">
+                        The library team would love to know what you have been reading.
+                        Share your favorite books, recent reads, and recommendations
+                        with us to help build a vibrant reading community!
+                    </p>
+
+                    <a href="#" className="btn main-color btn-lg text-white">
+                        Sign-up
+                    </a>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ 03-frontend/library-project/src/layouts/Homepage/LibraryServices.tsx
@@ -0,0 +1,23 @@
+export const LibraryServices = () => {
+    return(
+        <div className="container my-5">
+            <div className="row p-4 align-items-center border shadows-lg">
+                <div className="col-lg-7 p-3">
+                    <h1 className="display-4 fw-bold">
+                        Can't find what are you lokking for?
+                    </h1>
+                    <p className="lead">
+                        If you cannot find what are you looking for, leave a messege for out library admin!
+                    </p>
+                    <div className="d-grid gap-2 justify-content-md-start mb-4 mg-lg-3">
+                        <a href="#" className="btn main-color btn-lg text-white">
+                            Sign up
+                        </a>
+                    </div>
+                    </div>
+                    <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
+            </div>
+
+        </div>
+    );
+}
