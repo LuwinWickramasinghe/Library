b2efd8732ac59c6ad4b8a37b12c1efd072ef2bdf
iamluwin <luwin.anuka@gmail.com>
2024-07-25 02:12:04 +0530

    backend Configuration

---
+++ 02-backend/springboot-lib/src/test/java/com/luwan/springboot_lib/SpringbootLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luwan.springboot_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
