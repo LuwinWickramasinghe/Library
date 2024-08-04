59b2c111d78d209a6cf6bdf2a7dbe4dbaed3afcf
iamluwin <luwin.anuka@gmail.com>
2024-08-05 02:24:50 +0530

    Read only configuration

--- 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/dao/BookRepositary.java
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/dao/BookRepositary.java
@@ -4,4 +4,6 @@ import com.luwan.springboot_lib.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepositary extends JpaRepository<Book, Long> {
+
 }
+
