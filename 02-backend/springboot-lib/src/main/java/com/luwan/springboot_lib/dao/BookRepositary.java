947fb75381bec478d3dff03c3ca341314a3d680d
iamluwin <luwin.anuka@gmail.com>
2024-08-05 01:07:23 +0530

    create an interface

---
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/dao/BookRepositary.java
@@ -0,0 +1,7 @@
+package com.luwan.springboot_lib.dao;
+
+import com.luwan.springboot_lib.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepositary extends JpaRepository<Book, Long> {
+}
