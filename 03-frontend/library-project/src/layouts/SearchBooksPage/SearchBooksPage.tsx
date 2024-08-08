84a52e652ac01ba444c6fd4389a51fb3643fd79e
iamluwin <luwin.anuka@gmail.com>
2024-08-09 01:53:08 +0530

    view when there is no search results

--- 03-frontend/library-project/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/library-project/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -163,6 +163,8 @@ export const SearchBooksPage = () => {
               </div>
             </div>
           </div>
+          {totalAmountOfBooks > 0 ? 
+          <>
           <div className="mt-3">
             <h5>Number of results: ({totalAmountOfBooks})</h5>
           </div>
@@ -173,6 +175,15 @@ export const SearchBooksPage = () => {
           {books.map((book) => (
             <SearchBook book={book} key={book.id} />
           ))}
+          </>
+          :
+          <div className="m-5">
+            <h3>Can't find what you're looking for?</h3>
+            <a type="button" className="btn main-color btn-md px-4 me-md-2 fw-bold text-white" href="#">
+                Library Services
+            </a>
+          </div>
+            }
           {totalPages > 1 && (
             <Pagination
               currentPage={currentPage}
