67d2c40abb4cd5a100afad7f3529cc9f25fc125d
iamluwin <luwin.anuka@gmail.com>
2024-08-22 22:22:30 +0530

    Controller for checkout functionality

---
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.luwan.springboot_lib.controller;
+
+
+import com.luwan.springboot_lib.entity.Book;
+import com.luwan.springboot_lib.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@gmail.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
