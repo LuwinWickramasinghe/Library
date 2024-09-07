628d76338e76c903e3b1db1363c1a7ecdc036a79
iamluwin <luwin.anuka@gmail.com>
2024-09-08 00:16:34 +0530

    Message endpoint

---
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/controller/MessagesController.java
@@ -0,0 +1,28 @@
+package com.luwan.springboot_lib.controller;
+
+
+import com.luwan.springboot_lib.entity.Messages;
+import com.luwan.springboot_lib.service.MessagesService;
+import com.luwan.springboot_lib.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+            @RequestBody Messages messageRequest) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messagesService.postMessage(messageRequest, userEmail);
+    }
+}
