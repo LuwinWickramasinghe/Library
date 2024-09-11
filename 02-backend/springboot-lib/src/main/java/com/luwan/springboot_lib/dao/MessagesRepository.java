b236e948e22790ea021b552163a1bfe62ee97721
iamluwin <luwin.anuka@gmail.com>
2024-09-11 23:57:50 +0530

    End point for admin page messages

--- 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/dao/MessagesRepository.java
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/dao/MessagesRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessagesRepository extends JpaRepository<Message, Long> {
 
     Page<Message> findByUserEmail(@Param("userEmail") String userEmail, Pageable pageable);
+
+    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable pageable);
 }
