3ba194861ebec0410099e6fd7c67d1bbf0f3e91f
iamluwin <luwin.anuka@gmail.com>
2024-09-11 23:51:49 +0530

    Secure root and state based view

--- 03-frontend/library-project/src/layouts/ManageLibrary/ManageLibraryPage.tsx
+++ 03-frontend/library-project/src/layouts/ManageLibrary/ManageLibraryPage.tsx
@@ -5,8 +5,23 @@ export const ManageLibraryPage = () => {
 
     const { authState } = useOktaAuth();
 
-    const [ clickChangeQuantity, seClickChangeQuantity ] =  useState(false);
+    const [ changeBookQuantityClick, setChangeBookQuantityClick ] =  useState(false);
     const [ messagesClick,  setMessagesClick ] =  useState(false);
+
+    function addBookClick(){
+        setChangeBookQuantityClick(false);
+        setMessagesClick(false);
+    }
+
+    function clickChangeBookQuantity(){
+        setChangeBookQuantityClick(true);
+        setMessagesClick(false);
+    }
+
+    function clickMessages(){
+        setChangeBookQuantityClick(false);
+        setMessagesClick(true);
+    }
 
     return(
         <div className='container'>
@@ -14,19 +29,19 @@ export const ManageLibraryPage = () => {
             <h3>Manage Library</h3>
             <nav>
                 <div className='nav nav-tabs' id='nav-tab' role='tablist'>
-                    <button className='nav-link active' id='nav-add-book-tab' data-bs-toggle='tab' 
+                    <button onClick={addBookClick} className='nav-link active' id='nav-add-book-tab' data-bs-toggle='tab' 
                         data-bs-target='#nav-add-book' type='button' role='tab' aria-controls='nav-add-book' 
                         aria-selected='false'
                     >
                         Add new book
                     </button>
-                    <button className='nav-link' id='nav-quantity-tab' data-bs-toggle='tab' 
+                    <button onClick={clickChangeBookQuantity} className='nav-link' id='nav-quantity-tab' data-bs-toggle='tab' 
                         data-bs-target='#nav-quantity' type='button' role='tab' aria-controls='nav-quantity' 
                         aria-selected='true'
                     >
                         Change quantity
                     </button>
-                    <button className='nav-link' id='nav-messages-tab' data-bs-toggle='tab' 
+                    <button onClick={clickMessages} className='nav-link' id='nav-messages-tab' data-bs-toggle='tab' 
                         data-bs-target='#nav-messages' type='button' role='tab' aria-controls='nav-messages' 
                         aria-selected='false'
                     >
@@ -40,10 +55,10 @@ export const ManageLibraryPage = () => {
                         Add new book
                 </div>
                 <div className='tab-pane fade' id='nav-quantity' role='tabpanel' aria-labelledby='nav-quantity-tab'>
-                   Change Quantity
+                   {changeBookQuantityClick? <>Change Quantity</> : <></>}
                 </div>
                 <div className='tab-pane fade' id='nav-messages' role='tabpanel' aria-labelledby='nav-messages-tab'>
-                    Admin Messages
+                    {messagesClick ? <>Admin messages</> : <></>}
                 </div>
             </div>
         </div>
