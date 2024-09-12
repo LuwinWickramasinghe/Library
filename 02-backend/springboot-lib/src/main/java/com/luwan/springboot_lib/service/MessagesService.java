537c2cba061252df46497d55281bd756b56e995a
iamluwin <luwin.anuka@gmail.com>
2024-09-12 15:48:08 +0530

    admin page message response class and service

--- 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/service/MessagesService.java
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/service/MessagesService.java
@@ -3,9 +3,12 @@ package com.luwan.springboot_lib.service;
 
 import com.luwan.springboot_lib.dao.MessagesRepository;
 import com.luwan.springboot_lib.entity.Message;
+import com.luwan.springboot_lib.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -22,5 +25,17 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messagesRepository.save(message);
+    }
+
+    public void putResponse(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messagesRepository.findById(adminQuestionRequest.getId());
+        if (message.isEmpty()) {
+            throw new Exception("Message not found");
+        }
+
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messagesRepository.save(message.get());
     }
 }
