348a52a27963906f1ef64e4a459cb6097f1905f4
iamluwin <luwin.anuka@gmail.com>
2024-09-14 20:36:14 +0530

    controller endpoint for add book

---
+++ 02-backend/springboot-lib/src/main/java/com/luwan/springboot_lib/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.luwan.springboot_lib.controller;
+
+import com.luwan.springboot_lib.requestmodels.AddBookRequest;
+import com.luwan.springboot_lib.service.AdminService;
+import com.luwan.springboot_lib.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("api/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token,
+                        @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Access only for admins");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
